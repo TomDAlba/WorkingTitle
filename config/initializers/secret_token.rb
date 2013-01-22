0ba54c766c2be7d10bb58e339492ff543567f4ae
Andrea <me@iamues.com>
2013-01-22 13:23:24 +0100

    Update config/initializers/secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Simvla::Application.config.secret_token = '553428b7bc6b4ad7d9ab1a9f905db8088c49f3a10f2248ebd594544a453a8753aefcd980a9eaf2c03d6fec4e9983e3bb21fd46ee658645b0b94aed4c2fe1e13e'
+Simvla::Application.config.secret_token = ''
